3e00b208f47680531c7ce758c5f4d1ff915cb119
HarshdeepSinghKaint <harshdeepsinghkaint@gmail.com>
2024-05-15 20:02:47 +0530

    adding question and corresponding response of correct

--- Quiz.py
+++ Quiz.py
@@ -1,1 +1,36 @@
 print("Welcome to the Game :) ")
+
+answer=input("Do you want to play the game\nYes or no ?\n")
+
+
+if answer!="yes":
+    print("See you Again")
+    quit()
+
+
+print("Question number 1 ")
+answer=input("GPU stands for ?\n")
+if answer=="graphics processing unit":
+    print("Correct!")
+    
+print("Question number 2 ")
+answer=input("CPU stands for ?\n")
+if answer=="graphics processing unit":
+    print("Correct!")
+    
+print("Question number 3 ")
+answer=input("API stands for ?\n")
+if answer=="graphics processing unit":
+    print("Correct!")
+
+print("Question number 4 ")
+answer=input("DBMS stands for ?\n")
+if answer=="graphics processing unit":
+    print("Correct!")
+
+
+print("Question number 5 ")
+answer=input("RAM stands for ? ")
+if answer=="graphics processing unit":
+    print("Correct!")
+    
