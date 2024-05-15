02361e5acfbb0d58041cb6254b3a8752d11b82da
HarshdeepSinghKaint <harshdeepsinghkaint@gmail.com>
2024-05-15 20:08:44 +0530

    Adding some documentation

--- Quiz.py
+++ Quiz.py
@@ -1,20 +1,23 @@
+#Welcoming the Player to the game 
 print("Welcome to the Game :) ")
 
+#Asking player if he wants to play the game or not
 answer=input("Do you want to play the game\nYes or no ?\n")
 
-
+#if user says yes then we will continue with the game or else the game will end 
 if answer!="yes":
     print("See you Again")
     quit()
 
-
+#Question number1
 print("Question number 1 ")
 answer=input("GPU stands for ?\n")
 if answer=="graphics processing unit":
     print("Correct!")
 else:
     print("Incorrect")
-       
+    
+#Question number2      
 print("Question number 2 ")
 answer=input("CPU stands for ?\n")
 if answer=="graphics processing unit":
@@ -22,7 +25,7 @@ if answer=="graphics processing unit":
 else:
     print("Incorrect")
     
-    
+#Question number3    
 print("Question number 3 ")
 answer=input("API stands for ?\n")
 if answer=="graphics processing unit":
@@ -30,13 +33,15 @@ if answer=="graphics processing unit":
 else:
     print("Incorrect")
     
+#Question number4    
 print("Question number 4 ")
 answer=input("DBMS stands for ?\n")
 if answer=="graphics processing unit":
     print("Correct!")
 else:
     print("Incorrect")
-
+    
+#Question number5
 print("Question number 5 ")
 answer=input("RAM stands for ? ")
 if answer=="graphics processing unit":
